4392d70cbd9456cdc56dfd899852561f70976015
Simon Green <simon.green@thielsen.co.uk>
2019-02-02 12:40:20 +0000

    Added auth0 testing

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'home/show'
-  root 'home#show'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root "home#show"
+  get "/auth/oauth2/callback" => "auth0#callback"
+  get "/auth/failure" => "auth0#failure"
 end
